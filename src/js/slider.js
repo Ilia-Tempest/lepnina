036255464a7a85e14d1f8edcea24f5c7ae28c97c
Ilia-Tempest <51461710+Ilia-Tempest@users.noreply.github.com>
2020-10-13 17:53:57 +0500

    Закончил вёрстку главной страницы

---
+++ src/js/slider.js
@@ -0,0 +1,31 @@
+$('.portfolio-block').slick({
+  centerMode: true,
+  slidesToShow: 1,
+  focusOnSelect: true,
+  centerPadding: '60px',
+  arrows: false,
+  variableWidth: true,
+  infinite: true,
+  responsive: [
+    {
+      breakpoint: 800,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1,
+        variableWidth: false,
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        variableWidth: false,
+        slidesToShow: 1
+      }
+    }
+  ]
+});
