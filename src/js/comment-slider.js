314cdb6929a7ec3a1dc7b77666b9c6e1f33d5ec5
Ilia-Tempest <51461710+Ilia-Tempest@users.noreply.github.com>
2020-10-16 09:51:55 +0500

    Закончил основную вёрстку страниц

--- src/js/comment-slider.js
+++ src/js/comment-slider.js
@@ -23,4 +23,55 @@ $('.comment-block').slick({
   ]
 });
 
-	
+  
+$('.port-block').slick({
+  slidesToShow: 3,
+  focusOnSelect: true,
+  arrows: false,
+  infinite: true,
+  responsive: [
+    {
+      breakpoint: 1600,
+      settings: {
+        arrows: false,
+        slidesToShow: 2,
+        variableWidth: false,
+      }
+    },
+    {
+      breakpoint: 640,
+      settings: {
+        arrows: false,
+        variableWidth: false,
+        slidesToShow: 1
+      }
+    }
+  ]
+});
+
+
+
+$('.port-blocks').slick({
+  slidesToShow: 2,
+  focusOnSelect: true,
+  arrows: false,
+  infinite: true,
+  responsive: [
+    {
+      breakpoint: 1600,
+      settings: {
+        arrows: false,
+        slidesToShow: 2,
+        variableWidth: false,
+      }
+    },
+    {
+      breakpoint: 640,
+      settings: {
+        arrows: false,
+        variableWidth: false,
+        slidesToShow: 1
+      }
+    }
+  ]
+});
